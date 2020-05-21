16776ab42f21cd3269f9e73ea604df4f3edbca03
Jun Izumida <jun-izumida@agc.com>
2020-05-21 18:25:07 +0900

    [Feature] - tmux.conf

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/dotfiles/_zshrc ~/.zshrc
+ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 
-chsh -s /bin/zsh
+#chsh -s /bin/zsh
